63cb473ca23ece7d764f9fe1ce82c9b734da07a9
Gerald Reyes <gerhuete@gmail.com>
2022-12-13 21:44:57 -0600

    002-routes adding login route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import {AuthProvider} from './AuthContext'
 import {useState, useEffect} from 'react'
 import {auth} from './firebase'
 import {onAuthStateChanged} from 'firebase/auth'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
+import Profile from "./Profile";
 
 
 function App() {
@@ -20,10 +23,22 @@ function App() {
 
   return (
     <div className="App">
-       <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
-          <Login></Login>
+      <Router>
+        <AuthProvider value={{currentUser, timeActive, setTimeActive}}>
+        <Routes>
+          <Route exact path='/' element={
+            <PrivateRoute>
+              <Profile/>
+            </PrivateRoute>
+          }/>
+          <Route path="/login" element={
+            !currentUser?.emailVerified 
+            ? <Login/>
+            : <Navigate to='/' replace/>
+          } />
+        </Routes>
        </AuthProvider>
-     
+      </Router>
     </div>
   );
 }
